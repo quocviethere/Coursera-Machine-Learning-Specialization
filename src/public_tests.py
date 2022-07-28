47618d338dc4409b16999cb2b4f412fe38ef0e83
Quốc Việt <vietnguyen.31211027687@st.ueh.edu.vn>
2022-07-28 13:56:56 +0700

    Add files via upload

--- src/public_tests.py
+++ src/public_tests.py
@@ -1,67 +1,79 @@
+from tensorflow.keras.activations import relu, linear
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
 import numpy as np
 
-def compute_cost_test(target):
-    # print("Using X with shape (4, 1)")
-    # Case 1
-    x = np.array([2, 4, 6, 8]).T
-    y = np.array([7, 11, 15, 19]).T
-    initial_w = 2
-    initial_b = 3.0
-    cost = target(x, y, initial_w, initial_b)
-    assert cost == 0, f"Case 1: Cost must be 0 for a perfect prediction but got {cost}"
-    
-    # Case 2
-    x = np.array([2, 4, 6, 8]).T
-    y = np.array([7, 11, 15, 19]).T
-    initial_w = 2.0
-    initial_b = 1.0
-    cost = target(x, y, initial_w, initial_b)
-    assert cost == 2, f"Case 2: Cost must be 2 but got {cost}"
-    
-    # print("Using X with shape (5, 1)")
-    # Case 3
-    x = np.array([1.5, 2.5, 3.5, 4.5, 1.5]).T
-    y = np.array([4, 7, 10, 13, 5]).T
-    initial_w = 1
-    initial_b = 0.0
-    cost = target(x, y, initial_w, initial_b)
-    assert np.isclose(cost, 15.325), f"Case 3: Cost must be 15.325 for a perfect prediction but got {cost}"
-    
-    # Case 4
-    initial_b = 1.0
-    cost = target(x, y, initial_w, initial_b)
-    assert np.isclose(cost, 10.725), f"Case 4: Cost must be 10.725 but got {cost}"
-    
-    # Case 5
-    y = y - 2
-    initial_b = 1.0
-    cost = target(x, y, initial_w, initial_b)
-    assert  np.isclose(cost, 4.525), f"Case 5: Cost must be 4.525 but got {cost}"
-    
+def test_network(target):
+    num_actions = 4
+    state_size = 8
+    i = 0
+    assert len(target.layers) == 3, f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
+    assert target.input.shape.as_list() == [None, state_size], \
+        f"Wrong input shape. Expected [None,  400] but got {target.input.shape.as_list()}" 
+    expected = [[Dense, [None, 64], relu],
+                [Dense, [None, 64], relu],
+                [Dense, [None, num_actions], linear]]
+
+    for layer in target.layers:
+        assert type(layer) == expected[i][0], \
+            f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
+        assert layer.output.shape.as_list() == expected[i][1], \
+            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
+        assert layer.activation == expected[i][2], \
+            f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
+        i = i + 1
+
     print("\033[92mAll tests passed!")
     
-def compute_gradient_test(target):
-    print("Using X with shape (4, 1)")
-    # Case 1
-    x = np.array([2, 4, 6, 8]).T
-    y = np.array([4.5, 8.5, 12.5, 16.5]).T
-    initial_w = 2.
-    initial_b = 0.5
-    dj_dw, dj_db = target(x, y, initial_w, initial_b)
-    #assert dj_dw.shape == initial_w.shape, f"Wrong shape for dj_dw. {dj_dw} != {initial_w.shape}"
-    assert dj_db == 0.0, f"Case 1: dj_db is wrong: {dj_db} != 0.0"
-    assert np.allclose(dj_dw, 0), f"Case 1: dj_dw is wrong: {dj_dw} != [[0.0]]"
-    
-    # Case 2 
-    x = np.array([2, 4, 6, 8]).T
-    y = np.array([4, 7, 10, 13]).T + 2
-    initial_w = 1.5
-    initial_b = 1
-    dj_dw, dj_db = target(x, y, initial_w, initial_b)
-    #assert dj_dw.shape == initial_w.shape, f"Wrong shape for dj_dw. {dj_dw} != {initial_w.shape}"
-    assert dj_db == -2, f"Case 1: dj_db is wrong: {dj_db} != -2"
-    assert np.allclose(dj_dw, -10.0), f"Case 1: dj_dw is wrong: {dj_dw} != -10.0"   
-    
+def test_optimizer(target, ALPHA):
+    assert type(target) == Adam, f"Wrong optimizer. Expected: {Adam}, got: {target}"
+    assert np.isclose(target.learning_rate.numpy(), ALPHA), f"Wrong alpha. Expected: {ALPHA}, got: {target.learning_rate.numpy()}"
     print("\033[92mAll tests passed!")
+    
+    
+def test_compute_loss(target):
+    num_actions = 4
+    def target_q_network_random(inputs):
+        return np.float32(np.random.rand(inputs.shape[0],num_actions))
+    
+    def q_network_random(inputs):
+        return np.float32(np.random.rand(inputs.shape[0],num_actions))
+    
+    def target_q_network_ones(inputs):
+        return np.float32(np.ones((inputs.shape[0], num_actions)))
+    
+    def q_network_ones(inputs):
+        return np.float32(np.ones((inputs.shape[0], num_actions)))
+    
+    np.random.seed(1)
+    states = np.float32(np.random.rand(64, 8))
+    actions = np.float32(np.floor(np.random.uniform(0, 1, (64, )) * 4))
+    rewards = np.float32(np.random.rand(64, ))
+    next_states = np.float32(np.random.rand(64, 8))
+    done_vals = np.float32((np.random.uniform(0, 1, size=(64,)) > 0.96) * 1)
+
+    loss = target((states, actions, rewards, next_states, done_vals), 0.995, q_network_random, target_q_network_random)
     
 
+    assert np.isclose(loss, 0.6991737), f"Wrong value. Expected {0.6991737}, got {loss}"
+
+    # Test when episode terminates
+    done_vals = np.float32(np.ones((64,)))
+    loss = target((states, actions, rewards, next_states, done_vals), 0.995, q_network_ones, target_q_network_ones)
+    assert np.isclose(loss, 0.343270182), f"Wrong value. Expected {0.343270182}, got {loss}"
+      
+    # Test MSE with parameters A = B
+    done_vals = np.float32((np.random.uniform(0, 1, size=(64,)) > 0.96) * 1)
+    rewards = np.float32(np.ones((64, )))
+    loss = target((states, actions, rewards, next_states, done_vals), 0, q_network_ones, target_q_network_ones)
+    assert np.isclose(loss, 0), f"Wrong value. Expected {0}, got {loss}"
+ 
+    # Test MSE with parameters A = 0 and B = 1
+    done_vals = np.float32((np.random.uniform(0, 1, size=(64,)) > 0.96) * 1)
+    rewards = np.float32(np.zeros((64, )))
+    loss = target((states, actions, rewards, next_states, done_vals), 0, q_network_ones, target_q_network_ones)
+    assert np.isclose(loss, 1), f"Wrong value. Expected {1}, got {loss}"
+
+    print("\033[92mAll tests passed!")
+    
